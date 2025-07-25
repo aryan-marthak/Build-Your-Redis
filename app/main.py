14801db5f0adfb3e3506ed1f46fd9f52d241315f
Aryan Marthak <aryan.marthak1@gmail.com>
2025-07-26 00:15:43 +0530

    testing

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 import socket  # noqa: F401
+import threading
 
-
-def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() 
+def handle_client(connection):
     while True:
         data = connection.recv(1024)
         if b"PING" in data.upper():
             connection.sendall(b"+PONG\r\n")
+    
+
+def main():
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    while True:
+        connection, _ = server_socket.accept() 
+        thread = threading.Thread(target=handle_client, args=(connection,))
 
 
 if __name__ == "__main__":
