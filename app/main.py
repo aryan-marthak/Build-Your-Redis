f031f9b15b2bd007517595bcc44296b90b0d9c31
Aryan Marthak <aryan.marthak1@gmail.com>
2025-07-26 22:19:40 +0530

    testing

--- app/main.py
+++ app/main.py
@@ -1,20 +1,49 @@
 import socket  # noqa: F401
-import threading
+import selectors
 
-def handle_client(connection):
-    while True:
-        data = connection.recv(1024)
-        if b"PING" in data.upper():
-            connection.sendall(b"+PONG\r\n")
-    
+sel = selectors.DefaultSelector()
+
+def accept(sock):
+    conn, _ = sock.accept()
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read)
+
+def read(conn):
+    data = conn.recv(1024)
+    if b"PING" in data.upper():
+        conn.sendall(b"+PONG\r\n")
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.setblocking(False)
+    sel.register(server_socket, selectors.EVENT_READ, accept)
+    
     while True:
-        connection, _ = server_socket.accept() 
-        thread = threading.Thread(target=handle_client, args=(connection,))
-        thread.start()
-
+        events = sel.select()
+        for key, _ in events:
+            callback = key.data
+            callback(key.fileobj)
 
 if __name__ == "__main__":
     main()
+
+# import socket  # noqa: F401
+# import threading
+
+# def handle_client(connection):
+#     while True:
+#         data = connection.recv(1024)
+#         if b"PING" in data.upper():
+#             connection.sendall(b"+PONG\r\n")
+    
+
+# def main():
+#     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+#     while True:
+#         connection, _ = server_socket.accept() 
+#         thread = threading.Thread(target=handle_client, args=(connection,))
+#         thread.start()
+
+
+# if __name__ == "__main__":
+#     main()
